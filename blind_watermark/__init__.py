1cfd5ca87648940c1b66271d9cb446800459d32b
guofei9987 <guofei9987@foxmail.com>
2026-03-25 17:09:21 +0800

    prepare for version 0.6.x

--- blind_watermark/__init__.py
+++ blind_watermark/__init__.py
@@ -2,4 +2,6 @@ from .blind_watermark import WaterMark
 from .bwm_core import WaterMarkCore
 from .att import *
 from .recover import recover_crop
-from .version import __version__, bw_notes
+from .version import  bw_notes
+
+__version__ = '0.5.1'
